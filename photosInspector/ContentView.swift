d430dc9ec7b33a97df752d1127c8dbb6a0d3a2d1
Intelligencecore <07wood.temblor@icloud.com>
2026-04-22 23:36:07 -0700

    Second Commit for setup

---
+++ photosInspector/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  photosInspector
+//
+//  Created by Richier on 4/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+	var body: some View {
+	
+		
+		Text("This is a Placeholder screen")
+		Image(systemName:"iphone")
+			.font(.largeTitle)	
+		}
+		
+		
+	}
+
+
+#Preview {
+    ContentView()
+}
---
+++ photosInspector/photosInspectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  photosInspectorApp.swift
+//  photosInspector
+//
+//  Created by Richier on 4/22/26.
+//
+
+import SwiftUI
+
+@main
+struct photosInspectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
